6c40005003150fcf04a99adb6234c7440ffc2d79
Kacper <kacper.krzywiak88@gmail.com>
2020-02-28 00:25:31 +0100

    Added Category to db, model,repo,info

---
+++ RestaurantTemplate-API1/RestaurantTemplate-API1/Models/Enity/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantTemplate_API1.Models.Enity
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+    }
+}
